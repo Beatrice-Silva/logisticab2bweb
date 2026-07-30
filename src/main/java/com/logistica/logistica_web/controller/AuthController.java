860af89804e9c89694d7dd44bb7054f8c9ceea8b
Beatrice <beatricesilva1113@gmail.com>
2026-07-30 17:08:30 -0300

    Update authcont correcao

--- src/main/java/com/logistica/logistica_web/controller/AuthController.java
+++ src/main/java/com/logistica/logistica_web/controller/AuthController.java
@@ -4,13 +4,20 @@
  */
 package com.logistica.logistica_web.controller;
 
+import com.logistica.logistica_web.model.LojaDTO;
+import com.logistica.logistica_web.model.PacoteDTO;
+import com.logistica.logistica_web.model.UserRequestDTO;
+import com.logistica.logistica_web.model.UsuarioDTO;
 import com.logistica.logistica_web.service.AuthRestClientService;
 import jakarta.servlet.http.HttpSession;
+import java.util.List;
 import java.util.Map;
 import static jdk.javadoc.doclet.DocletEnvironment.ModuleMode.API;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,38 +27,50 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author Aluno
  */
 @Controller
-@RequestMapping("/auth")
 public class AuthController {
     
-    AuthRestClientService rest;
+    @Autowired
+    private AuthRestClientService restService;
   
     @GetMapping("/") 
     public String home(
             HttpSession session
             ){
-        
-        Object token = session.getAttribute("token");
-        
-        if(token == null){
-            return "index";
-        }
-        
         return "index"; 
     }
     
-    @PostMapping("/login")
-    public String logar(@RequestParam String email, @RequestParam String senha, Model m){
-        /*
-        Map user = rest.postForObject(API+"/auth/login", Map.of("email",email,"senha",senha), Map.class);
-        m.addAttribute("user", user);
-*/
-    return "redirect:/dashboard";
+    @GetMapping("/login")
+    public String login(
+            Model model){
+        UserRequestDTO credenciais = new UserRequestDTO();
+        model.addAttribute("credenciais", credenciais);
+    return "/login";
     }
-  
+    
+    
+    @PostMapping("/logar")
+    public String logar(
+            @ModelAttribute UserRequestDTO credenciais,
+            HttpSession session){
+        
+        String token = restService.login(credenciais);
+        System.out.println("token" + token);
+        session.setAttribute("token", token);
+        
+    return "redirect:/";
+    }
+    
     @GetMapping("/cadastrar") 
     public String cadastrar(){
         
         return "cadastro"; 
+    }
+    
+    @GetMapping("/listar/pacote") 
+    public List<PacoteDTO> listarPacote(PacoteDTO pacote){
+        List<PacoteDTO> pacotes = listarPacote(pacote);
+        
+        return listarPacote(pacote); 
     }
     
     @GetMapping("/criar/pacote") 
@@ -60,22 +79,24 @@ public class AuthController {
         return "criar"; 
     }
     
-    
+    @GetMapping("/listar/loja") 
+    public List<LojaDTO> listarLoja(LojaDTO loja){ 
+        return listarLoja(loja); 
+    }
     
     @GetMapping("/criar/loja") 
-    public String criarLoja(){
-        
+    public String criarLoja(){ 
         return "criar";
     }
     
-
+    @GetMapping("/listar/loja") 
+    public List<UsuarioDTO> listarEntregador(UsuarioDTO entregador){
+        return listarEntregador(entregador); 
+    }
+    
     @GetMapping("/dashboard") 
     public String mapear(Model m){
-        //Map counts = rest.getForObject(API+"/dashboard", Map.class);
-        //m.addAttribute("counts", counts);
         return "dashboard";
     }
-
-    
 
 }
